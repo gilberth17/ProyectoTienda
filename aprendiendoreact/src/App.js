66f510050a69d03ec2b608c86a28faf961e76c37
GilberthRomero <gilberth-r.r@hotmail.com>
2021-11-21 20:20:57 -0500

    primer repositorio

---
+++ aprendiendoreact/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img pexelsphoto34229641 />
+      </header>
+    </div>
+  );
+}
+
+export default App;
